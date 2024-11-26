3ffd4170f3c82efb31d5cfd53885d32516138b9f
DongYeop <s01054830455@gmail.com>
2024-11-26 18:05:03 +0900

    refactor: 입력 안내 메세지 상수화

--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -6,22 +6,26 @@ import oncall.model.Worker;
 
 public class InputView {
 
+    private static final String READ_MONTH_DAY_MESSAGE = "비상 근무를 배정할 월과 시작 요일을 입력하세요> ";
+    private static final String READ_WEEKDAY_WORKER_MESSAGE = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+    private static final String READ_DAY_OFF_WORKER_MESSAGE = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ";
+
     public String[] readMonthAndDay() {
-        System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+        System.out.print(READ_MONTH_DAY_MESSAGE);
         String monthAndDay = Console.readLine();
 
         return InputConvertor.convertMonthAndDay(monthAndDay);
     }
 
     public List<Worker> readWeekdayWorkers() {
-        System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        System.out.print(READ_WEEKDAY_WORKER_MESSAGE);
         String workers = Console.readLine();
 
         return InputConvertor.convertWorkers(workers);
     }
 
     public List<Worker> readDayOffWorkers() {
-        System.out.print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        System.out.print(READ_DAY_OFF_WORKER_MESSAGE);
         String workers = Console.readLine();
 
         return InputConvertor.convertWorkers(workers);
