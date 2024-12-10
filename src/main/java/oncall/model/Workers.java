4b5a1d56cb02bc1b80c96d1aa84d40c0289243d6
DongYeop <s01054830455@gmail.com>
2024-12-10 19:52:43 +0900

    refactor: 평일, 휴일 근무자를 관리하는 Workers 객체 추가

--- src/main/java/oncall/model/Workers.java
+++ src/main/java/oncall/model/Workers.java
@@ -1,38 +1,12 @@
 package oncall.model;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import oncall.exception.ErrorMessage;
-
 public class Workers {
 
-    private final Deque<Worker> workers;
+    private final WeekdayWorkers weekdayWorkers;
+    private final WeekendWorkers weekendWorkers;
 
-    private Workers(Deque<Worker> workers) {
-        validateDuplicate(workers);
-        validateSize(workers);
-        this.workers = workers;
-    }
-
-    public static Workers of(Deque<Worker> workers) {
-        return new Workers(workers);
-    }
-
-    private void validateSize(Deque<Worker> workers) {
-        if (workers.size() > 35 || workers.size() < 5) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
-        }
-    }
-
-    private void validateDuplicate(Deque<Worker> workers) {
-        HashSet<Worker> nonDuplicateWorkerSet = new HashSet<>(workers);
-        if (nonDuplicateWorkerSet.size() != workers.size()) {
-            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
-        }
-    }
-
-    public Deque<Worker> getWorkers() {
-        return new ArrayDeque<>(this.workers);
+    public Workers(WeekdayWorkers weekdayWorkers, WeekendWorkers weekendWorkers) {
+        this.weekdayWorkers = weekdayWorkers;
+        this.weekendWorkers = weekendWorkers;
     }
 }
