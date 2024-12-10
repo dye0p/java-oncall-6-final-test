cecd59c3782162403866b72b762d1bb070907dff
DongYeop <s01054830455@gmail.com>
2024-12-10 19:38:29 +0900

    feat: 근무자 객체 Worker 객체 추가

---
+++ src/main/java/oncall/model/Worker.java
@@ -0,0 +1,41 @@
+package oncall.model;
+
+import java.util.Objects;
+import oncall.exception.ErrorMessage;
+
+public class Worker {
+
+    private final String name;
+
+    private Worker(String name) {
+        validateSize(name);
+        this.name = name;
+    }
+
+    private void validateSize(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getErrorMessage());
+        }
+    }
+
+    public static Worker from(String name) {
+        return new Worker(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Worker worker = (Worker) o;
+        return Objects.equals(name, worker.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
