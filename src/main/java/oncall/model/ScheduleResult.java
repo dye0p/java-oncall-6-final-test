3616bc80003c82fca4146624c71bd588abbd3ab1
DongYeop <s01054830455@gmail.com>
2024-12-11 10:38:36 +0900

    feat: 근무표를 저장하는 ScheduleResult 클래스 추가

---
+++ src/main/java/oncall/model/ScheduleResult.java
@@ -0,0 +1,36 @@
+package oncall.model;
+
+public class ScheduleResult {
+
+    private final int month;
+    private final int date;
+    private final String dayOfWeek;
+    private final String worker;
+
+    private ScheduleResult(int month, int date, String dayOfWeek, String worker) {
+        this.month = month;
+        this.date = date;
+        this.dayOfWeek = dayOfWeek;
+        this.worker = worker;
+    }
+
+    public static ScheduleResult of(int month, int date, String dayOfWeek, String worker) {
+        return new ScheduleResult(month, date, dayOfWeek, worker);
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public String getWorker() {
+        return worker;
+    }
+}
